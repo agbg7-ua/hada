14fa3d9bd7648410b47e97d5478829726780f0b8
agbg7 <agbg7@alu.ua.es>
2023-04-19 11:04:45 +0200

    48679841L-> Añadidos los atributos de ENComentario

--- TiendaVideojuegos/library/ENComentario.cs
+++ TiendaVideojuegos/library/ENComentario.cs
@@ -8,5 +8,11 @@ namespace library
 {
     class ENComentario
     {
+        private int _id_producto;
+        private int _id;
+        private string _id_usuario;
+        private DateTime _date;
+        private string _text;
+        private int _valoracion;
     }
 }
