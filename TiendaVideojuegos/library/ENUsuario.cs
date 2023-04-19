1b6fb8501b69dd3309ac02204da2820387e39c2a
agbg7 <agbg7@alu.ua.es>
2023-04-19 10:47:33 +0200

    48679841L-> Añadidos Usuario y Mensaje adicionalmente añadidos los atributos de Usuario

--- TiendaVideojuegos/library/ENUsuario.cs
+++ TiendaVideojuegos/library/ENUsuario.cs
@@ -8,5 +8,28 @@ namespace library
 {
     class ENUsuario
     {
+        private string _username;
+        private string _nombre;
+        private string _apellidos;
+        private string _email;
+        private string _contraseña;
+        private int _edad;
+        private string _calle;
+        private string _pueblo;
+        private string _provincia;
+        private string _codigo_postal;
+        private string _telefono;
+
+        public string username { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string email { get; set; }
+        public string contraseña { get; set; }
+        public int edad { get; set; }
+        public string calle { get; set; }
+        public string pueblo { get; set; }
+        public string provincia { get; set; }
+        public string codigo_postal { get; set; }
+        public string telefono { get; set; }
     }
 }
