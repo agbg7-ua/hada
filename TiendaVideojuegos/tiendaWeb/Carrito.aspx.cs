dc7bb56fd785626acb441294b83b7b7c9507bb66
Sbb55-ua <98946265+Sbb55-ua@users.noreply.github.com>
2023-05-05 17:30:13 +0100

    Update Carrito.aspx.cs

--- TiendaVideojuegos/tiendaWeb/Carrito.aspx.cs
+++ TiendaVideojuegos/tiendaWeb/Carrito.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using library;
 using System.Data;
 
-<<<<<<< HEAD
+
 
 
 namespace tiendaWeb
@@ -30,37 +30,3 @@ namespace tiendaWeb
             }
 		}
 	}
-=======
-namespace tiendaWeb
-{
-    public partial class Carrito : System.Web.UI.Page
-    {
-        ENLineaCarrito lcar = new ENLineaCarrito();
-        ENCarrito car = new ENCarrito();
-        ENUsuario u = new ENUsuario();
-
-        DataSet d = new DataSet();
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            // Provisional hasta que se termine usuario
-            u.username = "usuario 1";
-            u.readUsuario();
-            car.readCarritoByUser(u);
-
-            d = lcar.showLineasCarritoByCarrito(car);
-
-            if ((d.Tables.Count > 0) && (d.Tables[0].Rows.Count > 0))
-            {
-                GridView1.DataSource = d;
-                GridView1.DataBind();
-            }
-        }
-
-        protected void button_eliminar_OnClientClick(object sender, EventArgs e)
-        {
-            
-        }
-    }
->>>>>>> develop
-}
