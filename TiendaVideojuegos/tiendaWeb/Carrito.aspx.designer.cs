4d1b8c247b1a2b3f13790e5eb2f1d5a1e712d442
Sbb55-ua <98946265+Sbb55-ua@users.noreply.github.com>
2023-05-05 18:12:42 +0100

    Y8317372B update

--- TiendaVideojuegos/tiendaWeb/Carrito.aspx.designer.cs
+++ TiendaVideojuegos/tiendaWeb/Carrito.aspx.designer.cs
@@ -15,29 +15,13 @@ namespace tiendaWeb
     {
 
         /// <summary>
-<<<<<<< HEAD
-        /// Control listView.
-=======
         /// Control GridView1.
->>>>>>> develop
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-<<<<<<< HEAD
-        protected global::System.Web.UI.WebControls.ListView listView;
-
-        /// <summary>
-        /// Control textboxVacio.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label textboxVacio;
-=======
         protected global::System.Web.UI.WebControls.GridView GridView1;
->>>>>>> develop
     }
 }
+
