6afb920442acd63943b8ffc98d4cb9470a33f57d
Carolina <cf72@gcloud.ua.es>
2023-05-06 11:13:26 +0200

    X5435988F -> Comienzo página de administrador (Admin.aspx)

---
+++ TiendaVideojuegos/tiendaWeb/Admin.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using library;
+
+namespace tiendaWeb
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ddlTest_SelectedIndexChanged(this, e);
+            }
+        }
+
+        // Dropdownlist --> opción seleccionada
+        protected void ddlTest_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlTest.SelectedValue == "1")
+            {
+                Response.Redirect("CategoriaProducto.aspx");
+            }
+            else if (ddlTest.SelectedValue == "2")
+            {
+                Response.Redirect("Producto.aspx");
+            }
+            else if (ddlTest.SelectedValue == "3")
+            {
+                Response.Redirect("Desarrollador.aspx");
+            }
+            else if (ddlTest.SelectedValue == "4")
+            {
+                Response.Redirect("Pedido.aspx");
+            }
+            else
+            {
+                textboxVacio.Visible = true;
+                textboxVacio.Text = "Elija la opción que desee";
+            }
+        }
+    }
+}
---
+++ TiendaVideojuegos/tiendaWeb/Admin.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tiendaWeb
+{
+
+
+    public partial class Admin
+    {
+
+        /// <summary>
+        /// Control ddlTest.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlTest;
+
+        /// <summary>
+        /// Control textboxVacio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label textboxVacio;
+    }
+}
