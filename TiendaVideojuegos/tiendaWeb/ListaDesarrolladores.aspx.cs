d994882ddc0a308774cfae638b6d8b902aa0c046
Jorge Sanchez Pastor <jsp60@alu.ua.es>
2023-05-15 22:07:47 +0200

    Funcinamiento basico de Desarrolladores con la BBDD

--- TiendaVideojuegos/tiendaWeb/ListaDesarrolladores.aspx.cs
+++ TiendaVideojuegos/tiendaWeb/ListaDesarrolladores.aspx.cs
@@ -20,34 +20,57 @@ namespace tiendaWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ENDesarrollador en = new ENDesarrollador();
-            try
+            //if (!IsPostBack)
+            //{
+            //    foreach (ListViewItem item in ListView1.Items)
+            //    {
+            //        Button boton_lista = (Button)item.FindControl("boton_lista");
+            //        string commandArgument = boton_lista.CommandArgument;
+            //        Page.ClientScript.RegisterForEventValidation(boton_lista.UniqueID, commandArgument);
+            //    }
+            //}
+
+            if (!IsPostBack)
             {
+                ENDesarrollador en = new ENDesarrollador();
+                try
+                {
 
 
-                en = en.obtener_todos().First();
-
-                List<ENDesarrollador> list = new List<ENDesarrollador>();
-                list = en.obtener_todos();
-               
-                
-
-                //List<Item2> list = new List<Item2>();
-                //list.Add(new Item2() { Nombre = "Bungie", Fecha = DateTime.Now.ToString().Split(' ')[0], Pais="Alemania" , ImagenURL="Imagenes/uno.png"});
-                //list.Add(new Item2() { Nombre = "Anapurna ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/ana.png"});
-                //list.Add(new Item2() { Nombre = "Desarrolladora 2 ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/sinimagen.jpeg" });
-                //list.Add(new Item2() { Nombre = "Bungie", Fecha = DateTime.Now.ToString().Split(' ')[0], Pais = "Alemania", ImagenURL = "Imagenes/uno.png" });
-                //list.Add(new Item2() { Nombre = "Anapurna ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/ana.png" });
-                //list.Add(new Item2() { Nombre = "Desarrolladora 2 ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/sinimagen.jpeg" });
+                    List<ENDesarrollador> list = new List<ENDesarrollador>();
+                    list = en.obtener_todos();
 
 
-                ListView1.DataSource = list;
-                ListView1.DataBind();
-            } 
-            catch (Exception ex)
-            {
-                Label_error.Text = ex.Message;
+
+                    //List<Item2> list = new List<Item2>();
+                    //list.Add(new Item2() { Nombre = "Bungie", Fecha = DateTime.Now.ToString().Split(' ')[0], Pais="Alemania" , ImagenURL="Imagenes/uno.png"});
+                    //list.Add(new Item2() { Nombre = "Anapurna ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/ana.png"});
+                    //list.Add(new Item2() { Nombre = "Desarrolladora 2 ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/sinimagen.jpeg" });
+                    //list.Add(new Item2() { Nombre = "Bungie", Fecha = DateTime.Now.ToString().Split(' ')[0], Pais = "Alemania", ImagenURL = "Imagenes/uno.png" });
+                    //list.Add(new Item2() { Nombre = "Anapurna ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/ana.png" });
+                    //list.Add(new Item2() { Nombre = "Desarrolladora 2 ", Fecha = DateTime.Now.AddDays(700).ToString().Split(' ')[0], Pais = "EEUU", ImagenURL = "Imagenes/sinimagen.jpeg" });
+
+
+                    ListView1.DataSource = list;
+                    ListView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    Label_error.Text = ex.Message;
+                }
             }
+
+        }
+
+
+
+        protected void test(object sender, EventArgs e)
+        {
+            ImageButton b = (ImageButton)sender;
+            string nombre = b.CommandArgument;
+            Session["desarrollador"] = nombre;
+            // redirect with the name as parameter to the next page
+            Response.Redirect("Desarrollador.aspx");
         }
     }
 }
