175d0641917c22b9c96f6d7b0394bb1de8ef14aa
Jorge Sanchez Pastor <jsp60@alu.ua.es>
2023-05-18 12:23:28 +0200

    48779447N Creada pagina en Admin para desarrolladores

---
+++ TiendaVideojuegos/tiendaWeb/AdminPáginas/DesarrolladorAdmin.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tiendaWeb.AdminPáginas
+{
+
+
+    public partial class DesarrolladorAdmin
+    {
+
+        /// <summary>
+        /// Control añadir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton añadir;
+
+        /// <summary>
+        /// Control listView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView listView1;
+
+        /// <summary>
+        /// Control textboxVacio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label textboxVacio;
+    }
+}
